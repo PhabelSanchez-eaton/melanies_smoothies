864d87f5e2abb2d5b4da1f6fcebaf01d20586aca
PhabelSanchez-eaton <albertopsanchez@eaton.com>
2025-07-15 16:46:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ if ingreients_list:
     ingredients_string = ''
     for fruit_chosen in ingreients_list:
         ingredients_string += fruit_chosen + ' '
-      smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
     #st.write(ingredients_string)
